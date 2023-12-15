09f552b4559e41f260b63043e50ffad774177e3c
ArtLevel <algitit@rambler.ru>
2023-12-15 17:00:07 +0300

    Task completed: Counter II

--- 30 days of JavaScript/thirtyDaysOfJavaScript.js
+++ 30 days of JavaScript/thirtyDaysOfJavaScript.js
@@ -39,3 +39,53 @@ counter()
  * counter() // 11
  * counter() // 12
  */
+
+// 2665. Counter II
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+
+const createCounter = function(init) {
+    let count = init
+
+    return {
+        increment: () => ++count,
+        decrement: () => --count,
+        reset: () => {
+            count = init
+            return init
+        },
+    }
+}
+
+class Counter {
+
+
+    constructor(init) {
+        this.init = init
+        this.count = init
+    }
+
+    increment() {
+        return ++this.count
+    }
+
+    decrement() {
+        return --this.count
+    }
+
+    reset() {
+        this.count = this.init
+        return this.count
+    }
+}
+
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
