2cfe94a59e0c420106483e3141008ccb4679e640
ArtLevel <algitit@rambler.ru>
2023-12-15 16:44:56 +0300

    Task completed: Create Counter

--- 30 days of JavaScript/thirtyDaysOfJavaScript.js
+++ 30 days of JavaScript/thirtyDaysOfJavaScript.js
@@ -1,3 +1,5 @@
+// 2667. Create Hello World Function
+
 /**
  * @return {Function}
  */
@@ -9,4 +11,31 @@ createHelloWorld()()
 /**
  * const f = createHelloWorld();
  * f(); // "Hello World"
+ */
+
+// 2620. Counter
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+
+const createCounter = (n) => {
+    let counter = n
+
+    return () => {
+        return counter++
+    }
+}
+
+const counter = createCounter(10)
+counter()
+counter()
+counter()
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
  */
